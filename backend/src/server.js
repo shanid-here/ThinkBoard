86a16909b3c3ecb42af11efc142619ebf6b9d432
Kingsman-007 <shanidmuhammed369@gmail.com>
2026-01-25 21:12:11 +0530

    prepared  for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 
 import notesRoutes from "./routes/notesRoutes.js"
@@ -17,13 +18,16 @@ dotenv.config()
 const app = express();
 const  PORT = process.env.PORT || 5001
 
+const __dirname = path.resolve()
 
 
 // middleware
-app.use(cors({
+if(process.env.NODE_ENV !=="production"){
+    app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+}
 app.use(express.json())
 app.use(rateLimiter)
 
@@ -36,6 +40,16 @@ app.use(cookieParser())
 app.use("/api/auth",auth)
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
+
 
 connectDB().then(() =>{
 app.listen(PORT, ()=>{
